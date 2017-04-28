2704f1ffced4e172af15f292dd6cc98f68c50b84
samilesma <samilesma@gmail.com>
2017-04-28 12:58:43 +0200

    StartEmptyPreset

--- dk.dtu.compute.mbse.tutorial.yawl/src/dk/dtu/compute/mbse/yawl/constraints/StartEmptyPreset.java
+++ dk.dtu.compute.mbse.tutorial.yawl/src/dk/dtu/compute/mbse/yawl/constraints/StartEmptyPreset.java
@@ -1,15 +1,26 @@
 package dk.dtu.compute.mbse.yawl.constraints;
 
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.validation.AbstractModelConstraint;
 import org.eclipse.emf.validation.IValidationContext;
+import org.pnml.tools.epnk.helpers.FlatAccess;
+import org.pnml.tools.epnk.helpers.NetFunctions;
+
+import dk.dtu.compute.mbse.yawl.Place;
 
 public class StartEmptyPreset extends AbstractModelConstraint{
 
 	@Override
 	public IStatus validate(IValidationContext ctx) {
-		// TODO Auto-generated method stub
-		return null;
+		EObject object = ctx.getTarget();
+		if(object instanceof Place){
+			FlatAccess fA =FlatAccess.getFlatAccess(NetFunctions.getPetriNet(object));
+			if(fA.getIn((Place) object).size()!=0){
+				return ctx.createFailureStatus(new Object[] {object});
+			}
+		}
+		return ctx.createSuccessStatus();
 	}
 
 }
